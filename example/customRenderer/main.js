6a4056be36a08ef649248219a03ea7f3cbea4188
liuqi <735611642@qq.com>
2022-04-30 10:31:38 +0800

    实现自定义渲染器custom renderer-3

--- example/customRenderer/main.js
+++ example/customRenderer/main.js
@@ -1,6 +1,5 @@
 import {App} from "./App.js";
-import {createRenderer} from "../../lib/guide-mini-vue.cjs";
-console.log(PIXI)
+import {createRenderer} from "../../lib/guide-mini-vue.esm.js";
 
 const game = new PIXI.Application({
     width:500,
@@ -9,14 +8,21 @@ const game = new PIXI.Application({
 document.body.append(game.view)
 const renderer = createRenderer({
     createElement(type){
-
+        if(type === 'rect'){
+            const rect = new PIXI.Graphics()
+            rect.beginFill(0xff0000)
+            rect.drawRect(0,0,100,100)
+            rect.endFill()
+            return rect
+        }
     },
-    pathProp(el,key,val){
-
+    patchProp(el,key,val){
+        el[key] = val
     },
     insert(el,parent){
-
+        parent.addChild(el)
     }
 })
+renderer.createApp(App).mount(game.stage)
 // const rootContainer = document.querySelector('#app')
 // createApp(App).mount(rootContainer)
