75649f634497b921e1c8200fbe9ef4be23c3d117
liuqi <735611642@qq.com>
2022-05-20 11:47:21 +0800

    实现transform功能

---
+++ src/compiler-core/src/transform.ts
@@ -0,0 +1,32 @@
+export function transform(root, options) {
+    //创建一个上下文存放root和options
+    const context = createTransformContext(root, options)
+    traverseNode(root, context)
+}
+
+function createTransformContext(root, options) {
+    return {
+        root,
+        nodeTransforms: options.nodeTransforms || []
+    }
+}
+
+//利用递归对ast树进行深度优先遍历
+function traverseNode(node, context) {
+    const {nodeTransforms} = context
+    //通过插件机制将容易变动的代码抽离出去，由外部去实现
+    //这样程序的扩展性就变得很强了，并且提高了程序的可测试性
+    for (const transform of nodeTransforms) {
+        transform(node)
+    }
+    traverseChildren(node, context)
+}
+
+function traverseChildren(node, context) {
+    const children = node.children
+    if (children) {
+        for (let i = 0; i < children.length; i++) {
+            traverseNode(children[i], context)
+        }
+    }
+}
---
+++ src/compiler-core/tests/transform.spec.ts
@@ -0,0 +1,19 @@
+import {baseParse} from "../src/parse";
+import {transform} from "../src/transform";
+import {NodeTypes} from "../src/ast";
+
+describe('transform', function () {
+    it('happy path', function () {
+        const ast = baseParse('<div>hi,{{message}}</div>')
+        const plugin = (node) => {
+            if (node.type === NodeTypes.TEXT) {
+                node.content += 'mini-vue'
+            }
+        }
+        transform(ast, {
+            nodeTransforms: [plugin]
+        })
+        const textNode = ast.children[0].children[0]
+        expect(textNode.content).toBe('hi,mini-vue')
+    });
+});
