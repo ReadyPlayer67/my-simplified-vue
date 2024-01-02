e3c2f3625091440692e57b0eb77f8b8d312de12f
liuqi <735611642@qq.com>
2024-01-02 10:25:47 +0800

    isReactive和isReadonly添加类型

--- packages/reactivity/src/reactive.ts
+++ packages/reactivity/src/reactive.ts
@@ -31,14 +31,14 @@ export const shallowReadonly = (raw: any) => {
   return createReactiveObject(raw, shallowReadonlyHandler, reactiveMap)
 }
 
-export const isReactive = (value) => {
+export const isReactive = (value: unknown): boolean => {
   //因为在proxy拦截的get操作里可以拿到isReadonly，所以只要触发get就可以判断isReactive，isReadonly同理
   //这里将结果转换为Boolean值，这样undefined就为false，就能通过这个方法检测原始对象
-  return !!value[ReactiveFlags.IS_REACTIVE]
+  return !!(value && value[ReactiveFlags.IS_REACTIVE])
 }
 
-export const isReadonly = (value) => {
-  return !!value[ReactiveFlags.IS_READONLY]
+export const isReadonly = (value: unknown): boolean => {
+  return !!(value && value[ReactiveFlags.IS_READONLY])
 }
 
 export const isProxy = (value) => {
