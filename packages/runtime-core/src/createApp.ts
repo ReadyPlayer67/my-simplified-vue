1af90a0175afdb813db413072ebc6cd34efec279
liuqi <735611642@qq.com>
2024-01-02 14:01:42 +0800

    修改createApp注释

--- packages/runtime-core/src/createApp.ts
+++ packages/runtime-core/src/createApp.ts
@@ -3,7 +3,7 @@ import {createVNode} from "./vnode";
 export function createAppApi(render){
     //接收一个根组件
     return function createApp(rootComponent) {
-        //返回一个对象，对象中有一个render方法
+        //返回一个对象，对象中有一个mount方法
         return {
             //render方法接收一个根容器
             mount(rootContainer) {
