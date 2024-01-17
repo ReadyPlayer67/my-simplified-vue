fc6894474e57d4c0986dc9f6a8d46988f3dcbac6
liuqi <735611642@qq.com>
2024-01-17 14:56:05 +0800

    为component实例添加类型

--- packages/runtime-core/src/component.ts
+++ packages/runtime-core/src/component.ts
@@ -5,18 +5,34 @@ import { emit } from './componentEmit'
 import { initSlots } from './componentSlots'
 import { VNode } from './vnode'
 
+export interface ComponentInternalInstance {
+  vnode: VNode
+  next: VNode | null
+  type: string | Symbol
+  update: Function
+  props: Record<string, unknown>
+  slots: any
+  setupState: Record<string, unknown>
+  emit: Function
+  isMounted: boolean
+  subTree: VNode
+  provides: Record<string, unknown>
+  parent: ComponentInternalInstance | null
+}
+
 export function createComponentInstance(vnode: VNode, parent) {
-  const instance = {
+  const instance: ComponentInternalInstance = {
     vnode,
     next: null,
     type: vnode.type,
-    update: null,
+    //null!表示初始赋值为null，但在后续处理中会对其进行赋值，明确不会为null，这样就无需将这个属性定义为可选属性
+    update: null!,
     props: {},
     slots: {},
     setupState: {},
     emit: () => {},
     isMounted: false,
-    subTree: {},
+    subTree: null!,
     //将parent.provides赋值给当前instance的provides实现跨组件传值
     provides: parent ? parent.provides : {},
     parent,
@@ -27,10 +43,11 @@ export function createComponentInstance(vnode: VNode, parent) {
   return instance
 }
 
-export function setupComponent(instance) {
+export function setupComponent(instance: ComponentInternalInstance) {
+  const { props, children } = instance.vnode
   //把vnode上的props挂载到组件instance上
-  initProps(instance, instance.vnode.props)
-  initSlots(instance, instance.vnode.children)
+  initProps(instance, props)
+  initSlots(instance, children)
   //初始化有状态的组件，与此相对的还有一个纯函数组件，是没有状态的
   setupStatefulComponent(instance)
 }
