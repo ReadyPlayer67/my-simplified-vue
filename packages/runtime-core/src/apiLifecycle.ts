07c42fa86588b153523f3f9b260d10167a99c2f4
liuqi <735611642@qq.com>
2024-01-20 22:16:20 +0800

    生命周期添加注释

--- packages/runtime-core/src/apiLifecycle.ts
+++ packages/runtime-core/src/apiLifecycle.ts
@@ -11,9 +11,11 @@ const createHook = (lifecycle: LifecycleHooks) => {
     target: ComponentInternalInstance | null = currentInstance
   ) => {
     if (target) {
+      //将当前组件实例设置为target，以便挂载生命周期函数
       const reset = setCurrentInstance(target)
       const hooks = target[lifecycle] || (target[lifecycle] = [])
       hooks.push(hook)
+      //挂载完之后记得还原组件实例为之前的组件实例
       reset()
     } else {
       console.error('声明周期函数只能在setup函数中使用')
